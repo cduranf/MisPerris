0de6b1e3814af07e551e39086ff5e2abb9b8bbbc
Carlos Durán <car.duranf@gmail.com>
2018-10-23 17:49:55 -0300

    Carga de html

--- mysite/MisPerris/tests.py
+++ mysite/MisPerris/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Post
+from django.utils import timezone
+
+class PostTestCase( TestCase ):
+
+    def test_postPublish( self ):
+        #Arrange
+        expected = size + 1
+        result = 0
+        #Act
+        
+        #Assert
+        self.assertEqual( expected, result)
+
 
 # Create your tests here.
