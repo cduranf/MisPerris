eb10ae56efcabb92f73425912c8cbf642a5a5384
Carlos Durán <car.duranf@gmail.com>
2018-10-26 11:23:11 -0300

    arreglos

--- MisPerris/MisPerris/urls.py
+++ MisPerris/MisPerris/urls.py
@@ -1,4 +1,4 @@
-"""mysite URL Configuration
+"""MisPerris URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
@@ -13,16 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path, include
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
-from users import views as user_views
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('MisPerris.urls')),
-    #modificado
-    path('registrar/', user_views.register, name='registrar'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
--- mysite/MisPerris/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MisperrisConfig(AppConfig):
-    name = 'MisPerris'
