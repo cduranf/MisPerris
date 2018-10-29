01bfc41b4ad11e6e30d90a702fd52f7fa5a5a4b8
Carlos Durán <car.duranf@gmail.com>
2018-10-29 11:11:08 -0300

    Carga de actualizaciones

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-from django.utils import timezone
-
 
 class Perro(models.Model):
     
@@ -19,5 +17,3 @@ class Perro(models.Model):
 
     def __str__(self):
         return self.Nombre
-
-
