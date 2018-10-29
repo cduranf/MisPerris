83d850a4547b145324c176e05c121c3425328cb9
Carlos Durán <car.duranf@gmail.com>
2018-10-28 23:01:41 -0300

    Carga ajustes pagina

--- my_project/my_project/urls.py
+++ my_project/my_project/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
-    path('signup/', TemplateView.as_view(template_name='signup.html'), name='signup'),
+   
     
     
 ]
